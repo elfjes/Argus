f14c83698fedc390c3fcd90cdd0978cfeadc437a
Pelle Koster <pelle.koster@geant.org>
2024-04-25 19:44:20 +0200

    keep only the bare minimum

--- src/argus/geant/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-from argus.incident.models import Incident
-
-# Create your models here.
--- src/argus/geant/views.py
+++
@@ -1,4 +0,0 @@
-from argus.incident.models import Incident
-from django.shortcuts import get_object_or_404, render
-
-...
